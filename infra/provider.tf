48a086b47b009ebf793603ff9b23d5341ba3ad62
sssandeep9999 <satyasaisandeep901@gmail.com>
2023-05-11 14:07:07 +0000

    infra

---
+++ infra/provider.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = "1.3.9"
+}
+
+# providers is nothing but aws,azure,gcp......
+
+provider "aws" {
+    version = "4.55.0"
+    region = "ap-south-1"
+}
---
+++ infra/variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_cidr_block" {}
+variable "psn1-cidr-block" {}
+variable "prn1-cidr-block" {}
+variable "az" {}
+variable "env" {}
